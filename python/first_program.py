805eee286285583f393444ecf24e3f1e4258ecf2
satyam-ksolves172024 <satyam.jha@ksolves.com>
2024-07-18 23:35:10 +0530

    testing git

--- python/first_program.py
+++ python/first_program.py
@@ -1,3 +1,3 @@
 strings_error = "Python strings cannot be changed — they are immutable. Therefore, assigning to an indexed position in the string results in an error:"
-
+var1 = 132334325
 print(strings_error)
